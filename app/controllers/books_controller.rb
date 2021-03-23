381ab4adc9ebf53a69ca52ea20cb1de862d0d038
eiji0111 <ee111fender@gmail.com>
2021-03-23 14:53:43 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,15 @@
 class BooksController < ApplicationController
+  def index
+    @users = User.all
+    @books = Book.all
+  end
+
   def create
     @book = Book.new
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
-  end
-
-  def index
-    @users = User.all
   end
 
   def show
